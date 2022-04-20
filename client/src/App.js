dcf3e5ced5033d10cfa14c122d94262dd649928b
Pavel <alyabev1998@gmail.com>
2022-04-20 19:11:28 +0300

    deleted git subrepo

---
+++ client/src/App.js
@@ -0,0 +1,39 @@
+import axios from "axios"
+import React, {useState} from "react";
+import './App.css';
+
+function App() {
+  var [sent, setSent] = useState(false)
+  var [text, setText] = useState("")
+  var [subject, setSubject] = useState("")
+
+  const handleSend = async() => {
+    
+    try{
+      await axios.post('http://localhost:3001/send_mail', {
+        text,
+        subject
+      })
+      setSent(true)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+return (
+  <div className="App">
+    {!sent ? (
+      <form onSubmit={handleSend}>
+        <input type="text" value={subject} onChange={(event) => setSubject(event.target.value)} />
+
+        <input type="text" value={text} onChange={(event) => setText(event.target.value)} />
+
+        <button type="submit">Send Email</button>
+      </form>
+    ) : (
+      <h1>Email Sent</h1>
+    )}
+  </div>
+)
+}
+export default App;
