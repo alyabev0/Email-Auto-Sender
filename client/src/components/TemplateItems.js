a74dd0201640974994039fe9c20306dcaefb2273
Pavel <alyabev1998@gmail.com>
2022-04-25 04:22:22 +0300

    fixed templates

--- client/src/components/TemplateItems.js
+++ client/src/components/TemplateItems.js
@@ -47,66 +47,9 @@ const TemplateItems = ({onChange, data}) => {
       </Flex>
     </React.Fragment>
     )
-
     return (
         <React.Fragment>
-{/* <Flex alignItems="center" justifyContent="center" maxH="40px" minW="171px" w="30%" >
-                <Flex alignItems="center" justifyContent="center" border="1px" borderBottomLeftRadius="7px" borderTopLeftRadius="7px"
-            borderTopRightRadius="none" borderBottomRightRadius="none" borderColor='gray.200' h="100%" w="40%" bgColor="#edf2f7">
-            <Text textAlign="center" display="flex" alignItems="center" justifyContent="center" h="100%" fontSize='xs'>Имя получателя:</Text>
-            </Flex>
-            <Input h="100%" w="60%" placeholder='Ввод...' size='xs' focusBorderColor='teal.400' onChange={onChange} value={data.receiverName} name="receiverName"
-            borderBottomLeftRadius="none" borderTopLeftRadius="none" borderTopRightRadius="7px" borderBottomRightRadius="7px"/>
-      </Flex>
-
-      <Flex alignItems="center" justifyContent="center" maxH="40px" minW="171px" w="30%" >
-                <Flex alignItems="center" justifyContent="center" border="1px" borderBottomLeftRadius="7px" borderTopLeftRadius="7px"
-            borderTopRightRadius="none" borderBottomRightRadius="none" borderColor='gray.200' h="100%" w="40%" bgColor="#edf2f7">
-            <Text textAlign="center" display="flex" alignItems="center" justifyContent="center" h="100%" fontSize='xs'>Ваша должность:</Text>
-            </Flex>
-            <Input h="100%" w="60%" placeholder='Ввод...' size='xs' focusBorderColor='teal.400' onChange={onChange} value={data.myPosition} name="myPosition"
-            borderBottomLeftRadius="none" borderTopLeftRadius="none" borderTopRightRadius="7px" borderBottomRightRadius="7px"/>
-      </Flex>
-
-      <Flex alignItems="center" justifyContent="center" maxH="40px" minW="171px" w="30%" >
-                <Flex alignItems="center" justifyContent="center" border="1px" borderBottomLeftRadius="7px" borderTopLeftRadius="7px"
-            borderTopRightRadius="none" borderBottomRightRadius="none" borderColor='gray.200' h="100%" w="40%" bgColor="#edf2f7">
-            <Text textAlign="center" display="flex" alignItems="center" justifyContent="center" h="100%" fontSize='xs'>Название компании:</Text>
-            </Flex>
-            <Input h="100%" w="60%" placeholder='Ввод...' size='xs' focusBorderColor='teal.400' onChange={onChange} value={data.myCompany} name="myCompany"
-            borderBottomLeftRadius="none" borderTopLeftRadius="none" borderTopRightRadius="7px" borderBottomRightRadius="7px"/>
-      </Flex>
-
-      <Flex alignItems="center" justifyContent="center" maxH="40px" minW="171px" w="30%" >
-                <Flex alignItems="center" justifyContent="center" border="1px" borderBottomLeftRadius="7px" borderTopLeftRadius="7px"
-            borderTopRightRadius="none" borderBottomRightRadius="none" borderColor='gray.200' h="100%" w="40%" bgColor="#edf2f7">
-            <Text textAlign="center" display="flex" alignItems="center" justifyContent="center" h="100%" fontSize='xs'>Ваше имя:</Text>
-            </Flex>
-            <Input h="100%" w="60%" placeholder='Ввод...' size='xs' focusBorderColor='teal.400' onChange={onChange} value={data.myName} name="myName"
-            borderBottomLeftRadius="none" borderTopLeftRadius="none" borderTopRightRadius="7px" borderBottomRightRadius="7px"/>
-      </Flex>
-
-      <Flex alignItems="center" justifyContent="center" maxH="40px" minW="171px" w="30%" >
-                <Flex alignItems="center" justifyContent="center" border="1px" borderBottomLeftRadius="7px" borderTopLeftRadius="7px"
-            borderTopRightRadius="none" borderBottomRightRadius="none" borderColor='gray.200' h="100%" w="40%" bgColor="#edf2f7">
-            <Text textAlign="center" display="flex" alignItems="center" justifyContent="center" h="100%" fontSize='xs'>Должность получателя:</Text>
-            </Flex>
-            <Input h="100%" w="60%" placeholder='Ввод...' size='xs' focusBorderColor='teal.400' onChange={onChange} value={data.rPosition} name="rPosition"
-            borderBottomLeftRadius="none" borderTopLeftRadius="none" borderTopRightRadius="7px" borderBottomRightRadius="7px"/>
-      </Flex>
-
-      <Flex alignItems="center" justifyContent="center" maxH="40px" minW="171px" w="30%" >
-                <Flex alignItems="center" justifyContent="center" border="1px" borderBottomLeftRadius="7px" borderTopLeftRadius="7px"
-            borderTopRightRadius="none" borderBottomRightRadius="none" borderColor='gray.200' h="100%" w="40%" bgColor="#edf2f7">
-            <Text textAlign="center" display="flex" alignItems="center" justifyContent="center" h="100%" fontSize='xs'>Ссылка:</Text>
-            </Flex>
-            <Input h="100%" w="60%" placeholder='Ввод...' size='xs' focusBorderColor='teal.400' onChange={onChange} value={data.link} name="link"
-            borderBottomLeftRadius="none" borderTopLeftRadius="none" borderTopRightRadius="7px" borderBottomRightRadius="7px"/>
-
-
-      </Flex> */}
       {listItems}
-      
         </React.Fragment>
     )
 }
--- client/src/pages/Templates.js
+++ client/src/pages/Templates.js
@@ -1,11 +1,12 @@
 import React, {useState, useEffect} from 'react'
-import { Flex, Text, Menu, MenuButton, MenuList, MenuItem, Button, Textarea, Input, InputGroup, InputLeftAddon, Center} from '@chakra-ui/react'
+import { Select, Flex, Text, Menu, MenuButton, MenuList, MenuItem, Button, Textarea, Input, InputGroup, InputLeftAddon, Center, BreadcrumbLink} from '@chakra-ui/react'
 import { ChevronDownIcon } from '@chakra-ui/icons'
 import LeftPanel from '../components/LeftPanel'
 import TemplateItems from '../components/TemplateItems'
 
 const Templates = () => {
   var [mainTemplate, setMainTemplate] = useState("")
+  var [activeOption, setActiveOption] = useState("")
   var [data, setData] = useState({
     myName: "*Ваше имя*",
     receiverName: "*Имя получателя*",
@@ -23,27 +24,59 @@ const Templates = () => {
   var template = {
     firstContact: `Здравствуйте, ${data.receiverName}!\r\nЯ нашел ваш профиль на ${data.link} и хотел поговорить об отличной возможности.\r\nЯ работаю в ${data.myCompany}, и мы ищем ${data.rPosition}. Я думаю, что Ваш опыт работы отлично подходит для этой позиции.\r\nЕсли вам интересно узнать больше, я буду рад пообщаться. Когда мы с Вами можем созвониться и обговорить детали?\r\nС нетерпением жду ответа от Вас в ближайшее время.\r\nС уважением,\r\n${data.myName},\r\n${data.myPosition}`,
     interviewInvite: `Здравствуйте, ${data.receiverName}!\r\nСпасибо, что подали заявку на вакансию ${data.rPosition} в ${data.myCompany}.\r\nМы рассмотрели Ваше резюме и будем рады продолжить общение.\r\nВ качестве продолжения общения я бы хотел пригласить Вас на небольшое собеседование. Собеседование продлится примерно полтора-два часа. Когда Вам будет удобно? Буду ждать Вашего ответа!\r\nС уважением,\r\n${data.myName},\r\n${data.myPosition}`,
-    jobOffer: `Здравствуйте, ${data.receiverName}!\r\nОт лица ${data.myCompany} спешу сообщить Вам о том, что Вы успешно прошли собеседование.\r\nВаши профессиональные взгляды сходятся со взглядами нашей компании, поэтому мы рады предложить Вам должность ${data.receiverName} в ${data.myCompany}.\r\nСкажите, как скоро Вы будете готовы дать ответ на наше предложение? В любом случае, ждём Вашего ответа с нетерпением!\r\nС уважением,\r\n${data.myName},\r\n${data.myPosition}`,
+    jobOffer: `Здравствуйте, ${data.receiverName}!\r\nОт лица ${data.myCompany} спешу сообщить Вам о том, что Вы успешно прошли собеседование.\r\nВаши профессиональные взгляды сходятся со взглядами нашей компании, поэтому мы рады предложить Вам должность ${data.rPosition} в ${data.myCompany}.\r\nСкажите, как скоро Вы будете готовы дать ответ на наше предложение? В любом случае, ждём Вашего ответа с нетерпением!\r\nС уважением,\r\n${data.myName},\r\n${data.myPosition}`,
     jobReject: `Здравствуйте, ${data.receiverName}!\r\nСпасибо, что подали заявку на нашу вакансию ${data.rPosition} и рассказали нашей команде о своем опыте. C сожалением сообщаем, что мы решили выбрать более опытного кандидата на эту роль.\r\nМы сохраним Ваше резюме в базе и свяжемся с Вами, если поймем, что Вы подходите для других вакансий, которые будут открыты в дальнейшем.\r\nМы искренне ценим Ваше время и внимание к ${data.myCompany}.\r\nВсего наилучшего,\r\nКоманда ${data.myCompany}`,
     requestTA: `Здравствуйте, ${data.receiverName}!\r\nЕще раз благодарим Вас за заявку на вакансию ${data.link}. Мы рассматриваем Ваше резюме и хотели бы увидеть примеры работ, чтобы лучше оценить ваши способности. Пожалуйста, ответьте на это письмо, приложив примеры Ваших работ.\r\nВсе файлы должны быть представлены в определённом формате. Если у Вас есть какие-либо вопросы, пожалуйста, дайте мне знать.\r\nС уважением,\r\n${data.myName},\r\n${data.myPosition}`
 }
-useEffect(() => {
-  // console.log(template.firstContact)
-  setMainTemplate(template.firstContact)
-  }, [template.firstContact]);
-useEffect(() => {
-  setMainTemplate(template.interviewInvite)
-  }, [template.interviewInvite]);
-useEffect(() => {
-  setMainTemplate(template.jobOffer)
-  }, [template.jobOffer]);
-useEffect(() => {
-  setMainTemplate(template.jobReject)
-  }, [template.jobReject]);
-useEffect(() => {
-  setMainTemplate(template.requestTA)
-  }, [template.requestTA]);
+// useEffect(() => {
+//   // console.log(template.firstContact)
+//   setMainTemplate(template.firstContact)
+//   }, [template.firstContact]);
+// useEffect(() => {
+//   setMainTemplate(template.interviewInvite)
+//   }, [template.interviewInvite]);
+// useEffect(() => {
+//   setMainTemplate(template.jobOffer)
+//   }, [template.jobOffer]);
 
+// useEffect(() => {
+//   setMainTemplate(template.requestTA)
+//   }, [template.requestTA]);
+//   useEffect(() => {
+//     setMainTemplate(template.jobReject)
+//     }, [template.jobReject]);
+function templateChoice (event) {
+console.log(event.target.id)
+setActiveOption(event.target.id)
+}
+function reload (event) {
+  console.log(activeOption)
+  // if (activeOption == 1){
+  //   setMainTemplate(template.firstContact)
+  // }
+switch(activeOption){
+  case "1":
+    console.log("yes")
+    setMainTemplate(template.firstContact)
+    break;
+    case "2":
+      setMainTemplate(template.interviewInvite)
+      break;
+      case "3":
+      setMainTemplate(template.jobOffer)
+      break;
+      case "4":
+      setMainTemplate(template.jobReject)
+      break;
+      case "5":
+      setMainTemplate(template.requestTA)
+      break;
+    default: 
+    break;
+}
+}
+// console.log(template.firstContact)
+// console.log(mainTemplate)
     return (
         <React.Fragment>
             <LeftPanel />
@@ -53,9 +86,9 @@ useEffect(() => {
           fontSize="50px" textAlign="center">Шаблоны писем</Text>
         </Flex>
         <Flex w="60%" minH="170px" h="20%"  minW="360px" direction="row" mt="5%"
-        boxShadow='sm' borderTopRightRadius="7px" borderTopLeftRadius="7px" flexWrap="wrap" justifyContent="space-between">
+         flexWrap="wrap" justifyContent="space-around">
             <TemplateItems data={data} onChange={dataChange}/>
-      <Menu>
+      {/* <Menu>
   <MenuButton minW="220px" w="30%" as={Button} rightIcon={<ChevronDownIcon />}>
     Выбрать шаблон
   </MenuButton>
@@ -66,7 +99,16 @@ useEffect(() => {
     <MenuItem onClick={() => setMainTemplate(template.jobReject)}>Письмо с отказом</MenuItem>
     <MenuItem onClick={() => setMainTemplate(template.requestTA)}>Письмо с ТЗ</MenuItem> 
   </MenuList>
-</Menu>
+</Menu> */}
+<Select placeholder='Выбрать шаблон' maxW="40%">
+  <option onClick={(event) => templateChoice(event)} id="1" value={template.firstContact}>Первый контакт</option>
+  <option onClick={(event) => templateChoice(event)} id="2" value={template.interviewInvite}>Приглашение на собеседование</option>
+  <option onClick={(event) => templateChoice(event)} id="3" value={template.jobOffer}>Предложение о работе</option>
+  <option onClick={(event) => templateChoice(event)} id="4" value={template.jobReject}>Письмо с отказом</option>
+  <option onClick={(event) => templateChoice(event)} id="5" value={template.requestTA}>Письмо с ТЗ</option>
+</Select>
+
+<Button w="40%" onClick={() => reload()}>Reload</Button>
 
         </Flex>
         <Flex w="60%" minH="200px" minW="360px" direction="column" 
