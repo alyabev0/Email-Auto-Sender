fc2b196e382f93b40f7adab36a18fd76d0d551bc
Pavel <alyabev1998@gmail.com>
2022-04-26 19:09:44 +0300

    added react helmet

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import MainPage from "./pages/MainPage"
 import AuthForm from "./pages/AuthForm";
 import Templates from "./pages/Templates";
+import Title from "./components/Title";
 import { Routes, Route } from "react-router-dom";
 import { Flex } from '@chakra-ui/react'
 
 function App() {
 
 return (
-  <div className="App">
+  <div>
+    <Title />
     <Flex w="100%" h="100vh" bg='white'>
     <Routes>        
         <Route path="/" element={(localStorage.getItem('isData') == "true" ) ? <MainPage /> : <AuthForm/> }/>
---
+++ client/src/components/Title.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {Helmet} from "react-helmet";
+
+class Title extends React.Component {
+  render () {
+    return (
+        <div>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>Email Auto Sender</title>
+            </Helmet>
+        </div>
+    );
+  }
+};
+export default Title
